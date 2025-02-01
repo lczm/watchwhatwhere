391c9f8591db621d3f80040244e5d5d19ce0a044
lczm <zemingchua@gmail.com>
2025-02-01 21:47:24 +0800

    add example env and api service

---
+++ web/src/utils/api.ts
@@ -0,0 +1,7 @@
+export class ApiService {
+  private static baseUrl = import.meta.env.VITE_API_BASE_URL;
+
+  static getEndpoint(path: string): string {
+    return `${this.baseUrl}${path}`;
+  }
+}
